f19526106e3952d3cd89f6ed79b21628dc2f7663
Sam5196 <sammedpachore1008@gmail.com>
2022-08-07 12:40:33 +0530

    Added:Test for sad mood

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -1,18 +1,13 @@
 package com.bridgelabz;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class MoodAnalyzerTest {
-    MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
     @Test
-    public void givenMessage_IsSad_ShouldReturnSad() {
+    public void givenMessage_IsProper_ShouldReturnSad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
         String actualResult = moodAnalyzer.analyseMood("I am Sad Mood");
         Assert.assertEquals("Sad", actualResult);
-    }
-    @Test
-    public void givenMessage_IsHappy_ShouldReturnHappy() {
-        String actualResult = moodAnalyzer.analyseMood("I am in Happy Mood");
-        Assert.assertEquals("Happy", actualResult);
     }
 }
