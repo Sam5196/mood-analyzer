98b83aea7d58f0c8886d52dcf04c2833cc2088de
Sam5196 <sammedpachore1008@gmail.com>
2022-08-07 11:43:27 +0530

    Welcome to mood analyzer program

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class MoodAnalyzer {
+    public String analyseMood(String message) {
+        if (message.toLowerCase().contains("happy")) {
+            return "Happy";
+        } else if (message.toLowerCase().contains("sad")) {
+            return "Sad";
+        } else {
+            return null;
+        }
+    }
+}
