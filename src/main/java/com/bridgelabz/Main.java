edefb14135be17d6505487ca05700c97699191bb
Sam5196 <sammedpachore1008@gmail.com>
2022-08-07 11:45:16 +0530

    Added:Logic to find word from from string and return respective string

--- src/main/java/com/bridgelabz/Main.java
+++ src/main/java/com/bridgelabz/Main.java
@@ -3,9 +3,9 @@ package com.bridgelabz;
 public class Main {
     public static void main(String[] args) {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyseMood("I am Happy");
+        String mood = moodAnalyzer.analyseMood("I am happy");
         System.out.println(mood);
-        mood = moodAnalyzer.analyseMood("I am Sad");
+        mood = moodAnalyzer.analyseMood("I am sad");
         System.out.println(mood);
     }
 }
