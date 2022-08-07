fcdba72aa14103ac8def59afa30aae09be995b74
Sam5196 <sammedpachore1008@gmail.com>
2022-08-07 12:47:49 +0530

    Added:Logic to handle null invalid mood

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,14 +1,41 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
-    public String analyseMood(String message) {
-        if (message.toLowerCase().contains("happy")) {
-            return "Happy";
-        } else if (message.toLowerCase().contains("sad")) {
-            return "Sad";
-        } else {
-            return null;
+    private String message;
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    /**
+     * Default constructor.
+     */
+    public MoodAnalyzer() {
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        try {
+            if (message.toLowerCase().contains("happy")) {
+                return "Happy";
+            } else if (message.toLowerCase().contains("sad")) {
+                return "Sad";
+            } else if (message.toLowerCase().contains("any")) {
+                return "Happy";
+            } else {
+                return "Any";
+            }
+        } catch (NullPointerException e) {
+            return "Invalid Mood";
         }
     }
+
 
 }
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -4,17 +4,37 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
+    MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+
     @Test
     public void givenMessage_IsSad_ShouldReturnSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String actualResult = moodAnalyzer.analyseMood("I am Sad Mood");
+        moodAnalyzer.setMessage("I am in Sad Mood");
+        String actualResult = moodAnalyzer.analyseMood();
         Assert.assertEquals("Sad", actualResult);
     }
 
     @Test
     public void givenMessage_IsHappy_ShouldReturnHappy() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String actualResult = moodAnalyzer.analyseMood("I am in Happy Mood");
+        moodAnalyzer.setMessage("I am in Happy Mood");
+        String actualResult = moodAnalyzer.analyseMood();
         Assert.assertEquals("Happy", actualResult);
+    }
+
+    /**
+     * Method to check if any is present in string, it should return Happy. Used the
+     * assert equals to check if the method output is happy.
+     */
+    @Test
+    public void givenMessage_IsAny_ShouldReturnHappy() {
+        moodAnalyzer.setMessage("I am in Any Mood");
+        String actualResult = moodAnalyzer.analyseMood();
+        Assert.assertEquals("Happy", actualResult);
+    }
+
+    @Test
+    public void givenMessage_IsInvalid_ShouldReturnInvalidMood() {
+        moodAnalyzer.setMessage(null);
+        String actualResult = moodAnalyzer.analyseMood();
+        Assert.assertEquals("Invalid Mood", actualResult);
     }
 }
